8e19b481b4043bff1afc29eb40778683535c8367
Chih-Ho Andy Chou <chihho.chou@wipro.com>
2018-10-19 12:14:32 -0400

    fix compilation error

--- src/test/java/hello/GreetingTest.java
+++ src/test/java/hello/GreetingTest.java
@@ -2,16 +2,12 @@ import junit.framework.TestCase;
 import hello;
 
 @RunWith(SpringRunner.class)
-public class GreetingTest extends TestCase {
-
-    public GreetingTest(String name) {
-            super( name );
-    }
+public class GreetingTest {
 
     @Test
 	public void whenGreeted_thenReturnMessage() {
 	    // given
-	    Greeting greet = new Greetibng(1, "Hye there");
+	    Greeting greet = new Greeting(1, "Hye there");
 	 
 	    // then
 	    assertThat(greet.getContent())
