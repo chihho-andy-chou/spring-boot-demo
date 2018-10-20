5b1aec85947a82976c98d0379261550708304b4c
Chih-Ho Andy Chou <chihho.chou@wipro.com>
2018-10-20 14:56:13 -0400

    try again

--- src/test/java/hello/GreetingTest.java
+++ src/test/java/hello/GreetingTest.java
@@ -1,5 +1,9 @@
-import hello;
 
+import hello.Greeting;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 public class GreetingTest {
