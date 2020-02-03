a4e0d31d83c4fe374bc3a7b25ea1001f9250dd2b
chihho-andy-chou <chihho.chou@wipro.com>
2020-02-03 12:32:03 -0500

    Refactored project structure

--- src/test/GreetingTest.java
+++ src/test/GreetingTest.java
@@ -1,5 +1,6 @@
+package test;
 
-import hello.Greeting;
+import main.hello.Greeting;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
